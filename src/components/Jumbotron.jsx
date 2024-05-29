8df0dcccbc6dde5b8b2ab4656403cd048d7a58f1
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-05-29 12:03:17 +0530

    search button made responsive

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -5,13 +5,24 @@ export const Jumbotron = () => {
   return (
     <div className="w-[100%] md:w-[75%] bg-red-200">
       <div className="flex flex-col p-5 justify-center items-center w-full py-10 gap-10">
-        <div className="flex flex-col p-5 justify-center items-start w-[100%] md:w-[95%]">
-          <h1 className="text-4xl md:text-5xl text-white">Welcome</h1>
+        <div className="flex flex-col p-5 justify-center md:items-start w-[100%] md:w-[95%] items-center gap-2 md:gap-0 text-center md:text-left">
+          <h1 className="text-4xl md:text-5xl text-white font-bold ">
+            Welcome.
+          </h1>
           <p className="text-xl md:text-3xl text-white">
             Millions of movies, TV shows and people to discover. Explore now.
           </p>
         </div>
-        <input type="text" className="w-[95%] py-3 rounded-3xl mb-[50px]" />
+        <div className="w-full sm:flex-row flex-col items-center flex justify-center sm:items-baseline relative">
+          <input
+            type="text"
+            className="w-[93%] py-3 rounded-3xl mb-[50px] outline-none pl-5 pr-7"
+            placeholder="Search for a movie, tv show, person......."
+          />
+          <button className="bg-gradient py-3 px-6 static sm:absolute right-5 rounded-3xl text-white font-bold">
+            Search
+          </button>
+        </div>
       </div>
 
       <div className="bg-blueGradient w-full flex flex-col p-10 gap-7 md:items-start items-center">
