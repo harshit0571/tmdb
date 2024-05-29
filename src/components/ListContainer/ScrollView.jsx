7af6a6bc376b49427ef5811fdfd59b3ba2d132c5
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-05-29 19:39:56 +0530

    created both popular section

--- src/components/ListContainer/ScrollView.jsx
+++ src/components/ListContainer/ScrollView.jsx
@@ -3,12 +3,13 @@ import Card from "./Card";
 
 const ScrollView = ({ data }) => {
   return (
-    <div className="w-full mb-5 flex justify-center items-center transition-full duration-200">
-      <div className="flex gap-5 overflow-auto pb-3 px-10">
+    <div className="w-full flex relative justify-center items-center transition-full duration-200">
+      <div className="flex gap-5 overflow-auto pb-3 px-10 ">
         {data.map((card) => (
           <Card key={data.id} data={card} />
         ))}
       </div>
+      <div className="absolute top-0 right-0 h-full w-10 bg-gradient-to-l from-white pointer-events-none"></div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,19 @@ const Home = () => {
         ]}
         data={data2}
       />
+
+      <DisplaySection
+        title="What's Popular"
+        tabs={[
+          { name: "movies", api: "/movie/popular?language=en-US&page=1" },
+          { name: "on tv", api: "/tv/popular?language=en-US&page=1" },
+          {
+            name: "in theatre",
+            api: "/movie/now_playing?language=en-US&page=1",
+          },
+        ]}
+        data={data2}
+      />
     </div>
   );
 };
