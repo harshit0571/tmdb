f3030d09bf5e743436d9b7c02d964b1ab175b412
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-06-05 13:03:23 +0530

    barel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React from "react";
 import {
+  React,
   Route,
   Routes,
-  BrowserRouter as Router,
+  Router,
   useLocation,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Movie from "./pages/Movie";
-import PersonWrapper from "./wrapper/PersonWrapper";
-import Login from "./pages/Login";
-import AuthProvider from "./context/AuthContext";
-import Bookmark from "./pages/Bookmark";
-import BookmarksProvider from "./context/BookmarksContext";
-import Favourites from "./pages/Favourites";
-import FavoritesProvider from "./context/FavouritesContext";
-import TV from "./pages/TV";
+  Home,
+  Navbar,
+  Footer,
+  Movie,
+  PersonWrapper,
+  Login,
+  AuthProvider,
+  Bookmark,
+  BookmarksProvider,
+  Favourites,
+  FavoritesProvider,
+  TV,
+} from "./Barrel";
 
 const AppContent = () => {
   const location = useLocation();
